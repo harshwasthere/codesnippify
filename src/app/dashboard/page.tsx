89d952ee7db9dd13352c3c2351e7170baf8a5ce5
harsh12codes <harsh12codes@gmail.com>
2024-09-10 10:33:36 +0530

    feat(ui): sidebar done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function DashboardPage() {
     return (
-        <div className="w-full h-full">
-            <div className="w-full h-14 p-2 border-b flex items-center"></div>
+        <div className="w-full h-full bg-muted/50">
+            <div className="w-full h-14 p-2 border-b flex items-center bg-background"></div>
         </div>
     );
 }
