e5b41b8a2e41ec97f94be38d0108695b67117522
harsh12codes <harsh12codes@gmail.com>
2024-11-10 10:12:46 +0530

    fixes

--- src/components/custom/forms/auth/LoginForm.tsx
+++ src/components/custom/forms/auth/LoginForm.tsx
@@ -98,7 +98,7 @@ export function LoginForm() {
                         }
                         className="w-full"
                     >
-                        {passwordLoginError && !loginError && (
+                        {passwordLoginPending && !loginError && (
                             <Loader className="mr-2 size-4 animate-spin" />
                         )}
                         Login
