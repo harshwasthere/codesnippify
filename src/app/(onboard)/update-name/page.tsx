166a3fc5c93204af1eddd0edf47d822bf311a274
harsh12codes <harsh12codes@gmail.com>
2025-01-08 09:39:07 +0530

    feat: Add update-name page component
    - Added a new file `src/app/(onboard)/update-name/page.tsx` which contains the component for the update name page in the onboarding flow.
    - The component includes a form for updating the user's name.
    - The page layout includes a logo, a heading, and a description.
    
    feat: Add update-password page component
    
    - Added a new file `src/app/(onboard)/update-password/page.tsx` which contains the component for the update password page in the onboarding flow.
    - The component includes a form for updating the user's password.
    - The page layout includes a logo, a heading, and a description.
    
    feat: Add confirm route component
    
    - Added a new file `src/app/auth/confirm/route.ts` which contains the component for the confirm route in the authentication flow.
    - The component handles the verification of an email OTP token and redirects the user accordingly.
    - If the verification is successful, the user is redirected to the specified next page.
    - If the verification fails, the user is redirected to an error page.
    
    feat: Add forgot-password page component
    
    - Added a new file `src/app/(auth)/forgot-password/page.tsx` which contains the component for the forgot password page in the authentication flow.
    - The component includes a form for requesting a password reset.
    - The page layout includes a logo and a heading.
    
    feat: Add signup page component
    
    - Added a new file `src/app/(auth)/signup/page.tsx` which contains the component for the signup page in the authentication flow.
    - The component includes a form for user registration.
    - The page layout includes a logo, a heading, and a description.
    
    feat: Add callback route component
    
    - Added a new file `src/app/auth/callback/route.ts` which contains the component for the callback route in the authentication flow.
    - The component handles the exchange of an authorization code for a session token.
    - If the exchange is successful, the user is redirected to the specified next page.
    - If the exchange fails, the user is redirected to an error page.
    
    feat: Add login page component
    
    - Added a new file `src/app/(auth)/login/page.tsx` which contains the component for the login page in the authentication flow.
    - The component includes a form for user login.
    - The page layout includes a logo, a heading, and links for signup and password recovery.
    
    feat: Add SearchInput component
    
    - Added a new file `src/components/custom/inputs/SearchInput.tsx` which contains the component for a search input field.
    - The component includes an input field with a search icon.
    - The input field has styling for various states such as focus, disabled, and placeholder text.
    
    fix: Minor code changes and fixes
    
    - Made minor changes and fixes to various components and files.
    - These changes include fixing types and making minor adjustments to components.

---
+++ src/app/(onboard)/update-name/page.tsx
@@ -0,0 +1,19 @@
+import { Icons } from "@/assets/icons";
+import { UpdateNameForm } from "@/components/custom/forms/onboard/UpdateNameForm";
+
+export default function LoginPage() {
+    return (
+        <main className="w-full h-screen flex items-center justify-center p-4">
+            <div className="max-w-96 w-full flex flex-col items-center justify-center space-y-4">
+                <div className="flex flex-col items-center gap-2">
+                    <Icons.LogoCodesnippify className="size-20" />
+                    <h1 className="text-4xl  font-extrabold">Your name?</h1>
+                    <p className="max-w-80 text-lg text-center text-muted-foreground">
+                        What would you like to be called?
+                    </p>
+                </div>
+                <UpdateNameForm />
+            </div>
+        </main>
+    );
+}
---
+++ src/components/custom/forms/auth/ForgotPasswordForm.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { ForgotPasswordFormSchema } from "@/lib/zod/schema";
+import { ForgotPasswordFormSchemaTypes } from "@/types/zod.types";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useSendResetPasswordLink } from "@/hooks/auth/useSendResetPasswordLink";
+import { Loader } from "lucide-react";
+
+export function ForgotPasswordForm() {
+    const form = useForm<ForgotPasswordFormSchemaTypes>({
+        resolver: zodResolver(ForgotPasswordFormSchema),
+        mode: "onChange",
+        defaultValues: {
+            email: "",
+        },
+    });
+    const { email } = form.watch();
+
+    const {
+        mutate: mutatesendResetPasswordLink,
+        isPending: sendResetPasswordLinkPending,
+        isError: sendResetPasswordLinkError,
+    } = useSendResetPasswordLink();
+
+    const handleSendResetPasswordLink = (data: ForgotPasswordFormSchemaTypes) => {
+        mutatesendResetPasswordLink(data);
+        form.reset();
+    };
+
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(handleSendResetPasswordLink)}
+                className="w-full space-y-4"
+            >
+                <FormField
+                    name="email"
+                    control={form.control}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Input
+                                    type="email"
+                                    placeholder="Email"
+                                    className="bg-secondary"
+                                    disabled={
+                                        sendResetPasswordLinkPending && !sendResetPasswordLinkError
+                                    }
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <Button
+                    type="submit"
+                    disabled={
+                        !form.formState.isValid ||
+                        !email ||
+                        (sendResetPasswordLinkPending && !sendResetPasswordLinkError)
+                    }
+                    className="w-full"
+                >
+                    {sendResetPasswordLinkPending && !sendResetPasswordLinkError && (
+                        <Loader className="mr-2 size-4 animate-spin" />
+                    )}
+                    Send reset link
+                </Button>
+            </form>
+        </Form>
+    );
+}
