b8867b08e2f66da4f6c45dbc45f8397a52c4ae02
harsh12codes <harsh12codes@gmail.com>
2025-01-22 23:56:08 +0530

    feat: home feature section

--- src/components/custom/home/HomeFeatureSection.tsx
+++ src/components/custom/home/HomeFeatureSection.tsx
@@ -1,8 +1,36 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { HomeFeatureCard } from "../cards/HomeFeatureCard";
+import HomeFeature1 from "../home-features/HomeFeature1";
+import HomeFeature2 from "../home-features/HomeFeature2";
+import HomeFeature3 from "../home-features/HomeFeature3";
+
+const features = [
+    {
+        showComponent: <HomeFeature1 />,
+        title: "Create, Save & Organize Snippets",
+        description:
+            "Effortlessly save your favorite code snippets and keep them neatly organized with folders, tags, and language-based categorization. Stay productive with everything you need at your fingertips.",
+    },
+    {
+        showComponent: <HomeFeature2 />,
+        title: "Secure & Private, with Public Folder Sharing",
+        description:
+            "Keep your private snippets safe and secure while easily sharing your collections with public folder links. Collaborate and share knowledge seamlessly with just a click.",
+    },
+    {
+        showComponent: <HomeFeature3 />,
+        title: "Multi-Language Support & Syntax Highlighting",
+        description:
+            "Manage code snippets in any programming language with built-in syntax highlighting. Use advanced filtering options to quickly find the exact snippet you need, when you need it.",
+    },
+];
 
 export function HomeFeatureSection() {
+    const [currentActiveFeature, setCurrentActiveFeature] = useState(0);
     return (
-        <section className="px-4 py-12">
+        <section className="w-full px-4 flex flex-col items-center">
             <div className="mx-auto max-w-3xl text-balance flex flex-col items-center text-center my-20 sm:my-28 z-10">
                 <div className="p-2 pb-4 text-3xl xs:text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-monaSans font-extrabold leading-none tracking-tighter text-balance m-0 bg-gradient-to-r from-foreground to-muted-foreground bg-clip-text text-transparent">
                     Features
@@ -12,110 +40,34 @@ export function HomeFeatureSection() {
                     folder links, and find snippets instantly with advanced search tools.
                 </p>
             </div>
-            <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
-                <div className="relative lg:row-span-2">
-                    <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
-                    <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
-                        <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
-                            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                                Mobile friendly
-                            </p>
-                            <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                                lorem cupidatat commodo.
-                            </p>
-                        </div>
-                        <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
-                            <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
-                                <Image
-                                    className="size-full object-cover object-top"
-                                    src="https://tailwindui.com/plus/img/component-images/bento-03-mobile-friendly.png"
-                                    alt=""
-                                    width={152}
-                                    height={152}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
-                </div>
-                <div className="relative max-lg:row-start-1">
-                    <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
-                    <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
-                        <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                                Performance
-                            </p>
-                            <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores
-                                impedit.
-                            </p>
-                        </div>
-                        <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
-                            <Image
-                                className="w-full max-lg:max-w-xs"
-                                src="https://tailwindui.com/plus/img/component-images/bento-03-performance.png"
-                                alt=""
-                                width={152}
-                                height={152}
+            <div className="mx-auto max-w-7xl w-full p-8">
+                <div className="w-full flex items-start gap-8">
+                    <div className="w-full space-y-8">
+                        {features.map((feature, index) => (
+                            <HomeFeatureCard
+                                key={index}
+                                title={feature.title}
+                                description={feature.description}
+                                setCurrentActiveFeature={setCurrentActiveFeature}
+                                index={index}
                             />
-                        </div>
+                        ))}
                     </div>
-                    <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
-                </div>
-                <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-                    <div className="absolute inset-px rounded-lg bg-white"></div>
-                    <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
-                        <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                                Security
-                            </p>
-                            <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                                Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse
-                                semper morbi.
-                            </p>
-                        </div>
-                        <div className="flex flex-1 items-center [container-type:inline-size] max-lg:py-6 lg:pb-2">
-                            <Image
-                                className="h-[min(152px,40cqw)] object-cover"
-                                src="https://tailwindui.com/plus/img/component-images/bento-03-security.png"
-                                alt=""
-                                width={152}
-                                height={152}
-                            />
-                        </div>
-                    </div>
-                    <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
-                </div>
-                <div className="relative lg:row-span-2">
-                    <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
-                    <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
-                        <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
-                            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                                Powerful APIs
-                            </p>
-                            <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                                Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget
-                                sem sodales gravida.
-                            </p>
-                        </div>
-                        <div className="relative min-h-[30rem] w-full grow">
-                            <div className="absolute bottom-0 left-10 right-0 top-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
-                                <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                                    <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
-                                        <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
-                                            NotificationSetting.jsx
-                                        </div>
-                                        <div className="border-r border-gray-600/10 px-4 py-2">
-                                            App.jsx
-                                        </div>
-                                    </div>
+                    <div className="sticky top-0 w-full flex h-[80vh] items-center">
+                        {features.map((feature, index) => {
+                            return (
+                                <div
+                                    className="w-full h-96 rounded-2xl hidden"
+                                    key={index}
+                                    style={{
+                                        display: currentActiveFeature === index ? "block" : "none",
+                                    }}
+                                >
+                                    {feature.showComponent}
                                 </div>
-                                <div className="px-6 pb-14 pt-6">{/* Your code example */}</div>
-                            </div>
-                        </div>
+                            );
+                        })}
                     </div>
-                    <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
                 </div>
             </div>
         </section>
