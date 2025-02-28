07653a9a90048c65b29f03491307c435b832d140
harsh12codes <harsh12codes@gmail.com>
2025-02-28 16:08:37 +0530

    chore: added features constants to constant.ts

---
+++ src/constants/constants.ts
@@ -0,0 +1,16 @@
+const homeFeatureSectionCode = `// Create and save a snippet
+const snippet = {
+  title: "Hello World",
+  language: "JavaScript",
+  code: "console.log('Hello, World!');",
+};
+
+// Save snippet
+saveSnippet(snippet);
+
+function saveSnippet(snippet) {
+  console.log(\`Snippet saved!\`);
+}
+`;
+
+export { homeFeatureSectionCode };
