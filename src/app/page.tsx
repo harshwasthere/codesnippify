8af4a0903dde172191f92b25c101c54e59dba346
harsh12codes <harsh12codes@gmail.com>
2025-02-28 19:11:48 +0530

    chore: depenencies, shadcn comp., config update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import { HomeNavbar } from "@/components/custom/home/Navbar";
 import { HomeHeroSection } from "@/components/custom/home/hero-section/HeroSection";
 import { HomeFeaturesSection } from "@/components/custom/home/features-section/FeaturesSection";
-
+import { HomePricingSection } from "@/components/custom/home/pricing-section/PricingSection";
+import { HomeFaqSection } from "@/components/custom/home/FaqSection";
+import { HomeCallToActionSection } from "@/components/custom/home/CallToActionSection";
 export default function Home() {
     return (
         <div className="min-h-screen h-full bg-background text-foreground scroll-smooth">
             <HomeNavbar />
             <HomeHeroSection />
             <HomeFeaturesSection />
+            <HomePricingSection />
+            <HomeFaqSection />
+            <HomeCallToActionSection />
         </div>
     );
 }
+
+// export default function Home() {
+//     return (
+//         <div className="min-h-screen h-full bg-background text-foreground scroll-smooth flex flex-col items-center justify-center">
+
+//         </div>
+//     );
+// }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,15 +63,40 @@ const config: Config = {
                     "5": "hsl(var(--chart-5))",
                 },
             },
-            screens: {
-                xs: "380px",
-                "2xs": "480px",
-                "3xl": "1800px",
+            boxShadow: {
+                glow: "0 -16px 128px 0 hsla(var(--brand-foreground) / 0.5) inset, 0 -16px 32px 0 hsla(var(--glow) / 0.5) inset",
             },
             borderRadius: {
                 lg: "var(--radius)",
                 md: "calc(var(--radius) - 2px)",
                 sm: "calc(var(--radius) - 4px)",
+            },
+            screens: {
+                xs: "380px",
+                "2xs": "480px",
+                "3xl": "1800px",
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: {
+                        height: "0",
+                    },
+                    to: {
+                        height: "var(--radix-accordion-content-height)",
+                    },
+                },
+                "accordion-up": {
+                    from: {
+                        height: "var(--radix-accordion-content-height)",
+                    },
+                    to: {
+                        height: "0",
+                    },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
             },
         },
     },
