f612f8f5f5a87f7bf9825b5db6334d8b4d57784d
harsh12codes <harsh12codes@gmail.com>
2025-03-03 09:18:13 +0530

    chore: schemas & types

---
+++ src/types/zod.types.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+import {
+    FolderCreateDialogSchema,
+    FolderRenameDialogSchema,
+    ForgotPasswordFormSchema,
+    LoginFormSchema,
+    ProfileSettingsFormSchema,
+    SignupFormSchema,
+    SnippetCreateFormSchema,
+    UpdateNameFormSchema,
+    UpdatePasswordFormSchema,
+} from "@/lib/schemas";
+
+// Authentication routes
+export type LoginFormSchemaTypes = z.infer<typeof LoginFormSchema>;
+export type SignupFormSchemaTypes = z.infer<typeof SignupFormSchema>;
+export type ForgotPasswordFormSchemaTypes = z.infer<
+    typeof ForgotPasswordFormSchema
+>;
+
+// Onboarding routes
+export type UpdatePasswordFormSchemaTypes = z.infer<
+    typeof UpdatePasswordFormSchema
+>;
+export type UpdateNameFormSchemaTypes = z.infer<typeof UpdateNameFormSchema>;
+
+// Folder related types
+export type FolderRenameDialogSchemaTypes = z.infer<
+    typeof FolderRenameDialogSchema
+>;
+export type FolderCreateDialogSchemaTypes = z.infer<
+    typeof FolderCreateDialogSchema
+>;
+
+// Profile settings related types
+export type ProfileSettingsFormSchemaTypes = z.infer<
+    typeof ProfileSettingsFormSchema
+>;
+
+// Snippet related types
+export type SnippetCreateFormSchemaTypes = z.infer<
+    typeof SnippetCreateFormSchema
+>;
